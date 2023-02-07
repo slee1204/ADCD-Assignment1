1bec94526fdca73efe6dbdcd1f0147fc3f00bc74
SueLee <slee1008@my.bcit.ca>
2023-02-06 22:31:14 -0800

    contact page without styling

---
+++ pages/contact.js
@@ -0,0 +1,55 @@
+import Head from "next/head";
+
+export default function Contact() {
+    return(
+        <>
+            <Head>
+                <title>Contact Us</title>
+                <meta name="author" content="MDIA 2109" />
+                <meta property="og:title" content="Assignment #1 - Contact Us Page" />
+                <meta
+                property="og:description"
+                content="BCIT Digital Design and Development Diploma"
+                />
+                <meta
+                property="viewport"
+                content="width=device-width, initial-scale=1"
+                />
+                <link rel="icon" href="/favicon.png" />
+            </Head>
+
+            <body>
+                <div>
+                    <h1>Contact Us</h1>
+                    <p>Want to discuss? Let's chat!</p>
+                </div>
+                <div>
+                    <form type="submit" style={{display: "flex", flexDirection: "column", maxWidth: "400px"}}>
+                        <fieldset>
+                            <table>
+                                <thead>First Name:
+                                    <tr>
+                                    <input type="text" id="firstName"  />
+                                    </tr>
+                                </thead>
+                                <thead>Last Name:
+                                    <tr>
+                                    <input type="text" id="lastName"  />
+                                    </tr>
+                                </thead>
+
+                                {/* <th>Email:
+                                    <tr><input type="text" id="email" /></tr>
+                                </th> */}
+
+                                
+                            </table>
+                            <button type="submit">Submit</button>
+                        </fieldset>
+                    </form>
+
+                </div>
+            </body>
+        </>
+    )
+}
