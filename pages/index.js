e8b4d58f817a0c897882c22936f3c7b8e718dc94
SueLee <slee1008@my.bcit.ca>
2023-02-06 18:45:00 -0800

    stylize home page

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,9 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/globals.css'
-
-
-const inter = Inter({ subsets: ['latin'] })
+import { useState } from 'react'
+import Menu from '@/comp/Menu'
 
 export default function Home() {
+
   return (
     <>
       <Head>
@@ -16,20 +13,52 @@ export default function Home() {
         <meta property="og:description" content="BCIT Digital Design and Development Diploma" />
         <meta property="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.png" />
+        <link rel="stylesheet" href="global.css" />
       </Head>
-      <main >
-        <h1>An investment in knowledge pays the best interest.</h1>
-        <p>Different than a college or university, the British Columbia Institute of Technology offers practical, flexible, applied education with instructors who have direct, hands-on experience in their field.</p>
-        <a href='/about'>
-          <button name='More About Us'>More About Us</button>
+
+      <nav className='navbar'>
+        <div className='hamburger'>
+          <img src="../icons/menu-icon.png" alt="menu icon" />
+          {/* <img src="../icons/menu-icon.png" alt="menu icon" />
+          <h1>X</h1>
+          <img src="../background-images/menuBackground.png"></img> */}
+
+        </div>
+        
+      </nav>
+      <main className='main'>
+
+        <div className='container'>
+
+          <div className='content'>
+            <h1>An investment in knowledge pays the best interest.</h1>
+            <hr/>
+            <p>Different than a college or university, the British Columbia Institute of Technology offers practical, flexible, applied education with instructors who have direct, hands-on experience in their field.</p>
+          </div>
+
+          <div className='buttons'>
+            <a href='/about' >
+              <button name='More About Us'>
+                More About Us
+              </button>
+            </a>
+
+            <a href='/contact'>
+              <button name='Contact Us'>
+                Contact Us
+              </button>
+            </a>
+          </div>
+
+        
+        </div>
+        
+        <a href='' className='arrow-down'>
+          <span><img src="/icons/downwardArrow.png" /></span>
         </a>
-        <a href='/contact'>
-          <button name='Contact Us'>Contact Us</button>
-        </a>
-        <a href=''>
-          <span><img src="/icons/downwardArrow.png"/></span>
-        </a>
+        
       </main>
+
     </>
   )
 }
